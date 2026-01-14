e4540bb2333864b64d233261eb3ed4d47236e2fa
phuentx <phuentx@gmail.com>
2026-01-14 23:27:29 +0600

    Update login.js

--- api/login.js
+++ api/login.js
@@ -1,11 +1,15 @@
+// api/login.js
+//I will add my admin SDK after learning about it
 export default function handler(req, res) {
-  if (req.method !== 'POST') return res.status(405).end();
-  res.status(200).json({
-    apiKey: "AIzaSyAJH79UQl0rc96Qug0DKLevO4ZI_sn8Kno",
+  const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY || "AIzaSyAJH79UQl0rc96Qug0DKLevO4ZI_sn8Kno",
     authDomain: "edulinki.firebaseapp.com",
     projectId: "edulinki",
     storageBucket: "edulinki.firebasestorage.app",
     messagingSenderId: "248886466474",
-    appId: "1:248886466474:web:160043201a6b28bafbbdd3"
-  });
+    appId: "1:248886466474:web:160043201a6b28bafbbdd3",
+    measurementId: "G-MQBH12VL7N",
+  };
+
+  res.status(200).json(firebaseConfig);
 }
